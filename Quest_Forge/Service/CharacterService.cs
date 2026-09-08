9424f77f423c28ca82bc3ef19e38ace5f9918bef
ZMehdiDev <z.mehdi.dev@gmail.com>
2026-09-09 00:11:16 +0200

    init: entity services

---
+++ Quest_Forge/Service/CharacterService.cs
@@ -0,0 +1,24 @@
+﻿using Quest_Forge.Entity;
+
+namespace Quest_Forge.Service;
+
+public class CharacterService(List<Character> characters)
+{
+    private readonly List<Character> _characters = characters;
+
+    public Character? FindCharacterById(int id)
+    {
+        foreach (var character in _characters)
+        {
+            if (character.Id == id)
+            {
+                return character;
+            }
+        }
+        Console.WriteLine("This chacater does not exists");
+        return null;
+    }
+    
+    
+    
+}
---
+++ Quest_Forge/Service/QuestService.cs
@@ -0,0 +1,21 @@
+﻿using Quest_Forge.Entity;
+
+namespace Quest_Forge.Service;
+
+public class QuestService(List<Quest> quests)
+{
+    private readonly List<Quest> _quest = quests;
+
+    public Quest? FindQuestById(int id)
+    {
+        foreach (var quest in _quest)
+        {
+            if (quest.Id == id)
+            {
+                return quest;
+            }
+        }
+        Console.WriteLine("This quest does not exists...");
+        return null;
+    }
+}
